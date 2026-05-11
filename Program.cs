118f1b4de5bef7d56d7e9ebbb078994c344a026c
le <le@example.com>
2026-05-11 22:37:40 +0700

    Fix: Remove invalid using namespace in Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using VibeCity_API.Data;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
